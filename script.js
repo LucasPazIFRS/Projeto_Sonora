c6a29061a140d3f2b32816e532939d5d256d6218
LucasPazIFRS <02150148@aluno.canoas.ifrs.edu.br>
2025-03-26 06:04:31 -0300

    Add files via upload
    Arquivos padrão provavelmente temporários

---
+++ script.js
@@ -0,0 +1,4 @@
+        function toggleTextBox() {
+            const textBox = document.getElementById('textBox');
+            textBox.style.display = textBox.style.display === 'block' ? 'none' : 'block';
+        }
