86bd6c46afc4d5dd89d51545b6f116608a301d66
LucasPazIFRS <02150148@aluno.canoas.ifrs.edu.br>
2025-04-28 15:26:05 -0300

    Apanhando pra css, de praxe

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,25 +2,27 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Header.scss';
-
+import { Link } from 'react-router-dom';
 export default function Header() {
   const navigate = useNavigate();
 
   return (
     <header className="header">
       <div className="header-buttons">
-        <button onClick={() => navigate(-1)}>&lt;</button>
-        <button onClick={() => navigate(1)}>&gt;</button>
+        <button className="header-button" onClick={() => navigate(-1)}>&lt;</button>
+        <button className="header-button" onClick={() => navigate(1)}>&gt;</button>
       </div>
       <div className="header-center">
-      <button onClick={() => navigate('/')}>
-        <h1>𝄞Sonora</h1>
-        </button>
+     
+      <Link to="/">
+  <h1>𝄞Sonora</h1>
+      </Link>
+     
       </div>
       <div className="header-right">
-        <button onClick={() => navigate('/profile')}>Perfil</button>
-        <button onClick={() => navigate('/configs')}>Configs</button>
-        <button onClick={() => navigate('/documentation')}>Docs</button>
+        <button className="header-button" onClick={() => navigate('/profile')}>Perfil</button>
+        <button className="header-button" onClick={() => navigate('/configs')}>Configs</button>
+        <button className="header-button" onClick={() => navigate('/documentation')}>Docs</button>
       </div>
     </header>
   );
