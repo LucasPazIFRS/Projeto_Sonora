f1ef0b11840c10895ccb9c242a926d63fe8c02d3
LucasPazIFRS <02150148@aluno.canoas.ifrs.edu.br>
2025-07-31 10:38:23 -0300

    Espaço pra ex's novo e mecânicas novas

--- Frontend/src/components/Counter/Counter.jsx
+++ Frontend/src/components/Counter/Counter.jsx
@@ -1,3 +1,1 @@
-
-//Aqui é um contador dos exercícios, o usuário pode clicar em quantos ele quer ser desafiado, a cada 10 que ele fizer sem errar ele ganha uma medalha,
-//o contador vai guardar os multiplicadores.
+//contador de escore do jogador
---
+++ Frontend/src/components/Modifier/Modifier.jsx
@@ -0,0 +1,7 @@
+//modificador da pontuação do jogador
+//TENTATIVAS: 5 , 10 , 20 , inf
+//Cada um botão das tentativas vai multiplicar a pontuação do jogador por x respectivamente
+//Ao concluir as tentativas, o jogador ganha uma medalha de acordo com a quantidade de tentativas que ele escolheu
+//há uma medalha secreta ao conseguir 35 tentativas no infinito
+
+//é possível tirar a partitura ou o som do exercício aqui, isso garante 2x a pontuação do jogador
