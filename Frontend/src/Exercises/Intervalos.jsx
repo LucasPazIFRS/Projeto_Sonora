e01b141e4cfba0939fb1058d06d78739549c9029
LucasPazIFRS <02150148@aluno.canoas.ifrs.edu.br>
2025-07-31 10:53:38 -0300

    Organização das pastas internas (ainda tenho que remover as notes.jsx mas elas tão dando problema quando retiradas,mesmoquemeudeusparadedigitar

--- Frontend/src/Exercises/Intervalos.jsx
+++
@@ -1,4 +0,0 @@
-//Intervalos são a distância entre duas notas musicais.
-// Eles são fundamentais para entender a música, pois definem a harmonia e a melodia
-// de uma peça musical. Neste exercício, você irá praticar o reconhecimento de intervalos
-// através de cliques em botões que representam diferentes intervalos. (obrigado copilot pelo texto)
